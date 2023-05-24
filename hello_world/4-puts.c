5763647165ad4c1f29a0c13e957ded4f76e10b11
Isaiah <6637@holbertonstudents.com>
2023-05-24 15:22:59 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,6 @@
 
 #include <stdio.h>
-
-/**
- * main - Entry point
- * return: Always 0 (sucess)
- * /
- 
-
 int main() {
-	puts("\"Programming is like building a multilingual puzzle)";
-	return 0:
+	puts("\"Programming is like building a multilingual puzzle)");
+	 return 0;;
 }
