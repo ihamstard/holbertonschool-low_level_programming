52bfbc9a28d6e2626fa4517d57f1419c3ec68ff8
Isaiah <6637@holbertonstudents.com>
2023-05-26 15:23:38 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ * Return: Alway (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
