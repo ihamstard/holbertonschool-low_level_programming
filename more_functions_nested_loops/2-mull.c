008862aec72b246e032971a3c7af0915f5d34942
Isaiah <6637@holbertonstudents.com>
2023-05-31 09:57:49 -0700

    2-mull.c

---
+++ more_functions_nested_loops/2-mull.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -Multiplies two integers
+ * @a: The first number to multiply
+ * @b: The second number to multiply with first number
+ * Return: The result or multiplication between a param and b param
+ */
+
+int mull(int a, int b)
+
+{
+	return (a * b);
+
+}
