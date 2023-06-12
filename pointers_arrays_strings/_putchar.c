2e60af7d953703852a73905303129bbc9cc80eb3
Isaiah <6637@holbertonstudents.com>
2023-06-12 15:49:40 -0700

    hyo

--- pointers_arrays_strings/_putchar.c
+++ pointers_arrays_strings/_putchar.c
@@ -8,6 +8,6 @@
  * On error, -1 is returned, and error is set appropriately                                    
  */                                                                                            
 int _putchar(char c)                                                                           
-{                                                                                              
-	        return (write(1, &c, 1));                                                              
+{
+	return (write(1, &c, 1));
 }  
