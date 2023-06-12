80abf193540436d4faa70d31b6c88be1a6fb44b8
Isaiah <6637@holbertonstudents.com>
2023-06-12 15:06:04 -0700

    23

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strchr - locate a character string
+ * @s: source of string
+ * @c: character to find
+ *
+ * Return: The string from character found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+	for (b = 0; b <= a; b++)
+	{
+	if (c == s[b])
+		{
+		s += b;
+	return (s);
+		}
+	}
+	return ('\0');
+}
