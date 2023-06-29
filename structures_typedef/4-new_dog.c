699d2e99d41c7ef43151d4f297402d33bc7dabdb
Isaiah <6637@holbertonstudents.com>
2023-06-29 07:33:13 -0700

    changes

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -64,7 +64,7 @@ int _strlen(char *s)
 	{
 		c++;
 	}
-	
+
 	return (c);
 }
 /**
@@ -80,10 +80,10 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src [i];
+		dest[i] = src[i];
 	}
-	
+
 	dest[i++] = '\0';
-	
+
 	return (dest);
 }
